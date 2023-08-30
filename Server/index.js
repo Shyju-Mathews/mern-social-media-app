70affc1ad4dd69b86a8ef24ed3d966e5c82c6601
Shyju-Mathews <91419372+Shyju-Mathews@users.noreply.github.com>
2023-08-30 10:13:38 +0530

    cors updated

--- Server/index.js
+++ Server/index.js
@@ -29,8 +29,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
   origin: ["https://mern-social-media-app-frontend-theta.vercel.app"],
-  method: ["POST", "GET", "PATCH", "DELETE"],
-  credentials: true
+  method: ["GET,POST,PUT,DELETE, PATCH"],
+  credentials: true,
+  maxAge: 3600
   }
   ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
